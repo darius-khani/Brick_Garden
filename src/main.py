0baa99198bf07f7dbc3c683c6e38924728736ebd
Darius Khani <smp2qg@virginia.edu>
2026-09-04 23:14:59 -0400

    More Setup

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 
+# Root Widget
 class BrickGarden(Widget):
     pass
 
+# Loads brickgarden.kv | Returns Root Widget
 class BrickGardenApp(App):
     def build(self):
         return BrickGarden()
 
+# Runs The Program | python src/main.py
 if __name__ == '__main__':
     BrickGardenApp().run()
