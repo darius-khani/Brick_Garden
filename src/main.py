33fe5627b6e729f3413ee8e50ab64d3b397e4654
Darius Khani <smp2qg@virginia.edu>
2026-09-06 15:34:58 -0400

    Added currency variable, fonts, and Asset Directory Reference

--- src/main.py
+++ src/main.py
@@ -4,10 +4,25 @@ Config.set('graphics', 'width', '402')
 Config.set('graphics', 'height', '874')   # Yes, it really is that long
 Config.set('graphics', 'resizable', False)
 
+# Kivy 
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
+from kivy.core.text import LabelBase
 
+# Screens
 from screens import HomeScreen
+
+# Path Set Up
+import os
+from kivy.resources import resource_add_path
+
+# Add Path References
+ASSETS_DIR = os.path.join(os.path.dirname(__file__), 'assets')
+resource_add_path(ASSETS_DIR)
+
+# Register Fonts
+LabelBase.register(name='IndieFlower', fn_regular=os.path.join(ASSETS_DIR, 'Indie_Flower/IndieFlower-Regular.ttf'))
+LabelBase.register(name='NanumBrush', fn_regular=os.path.join(ASSETS_DIR, 'Nanum_Brush_Script/NanumBrushScript-Regular.ttf'))
 
 # Builds App | Creates ScreenManager | Loads brickgarden.kv
 class BrickGardenApp(App):
--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -2,10 +2,12 @@ import os
 
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
+from kivy.properties import NumericProperty
 
 # Loads home_screen.kv | Register HomeScreen Rule
 Builder.load_file(os.path.join(os.path.dirname(__file__), 'home_screen.kv'))    # Strips 'homescreen.py' from path string and replaces it with 'home_screen.kv'
 
 # HomeScreen Widget
 class HomeScreen(Screen):
+    vitality = NumericProperty(10)
     pass
