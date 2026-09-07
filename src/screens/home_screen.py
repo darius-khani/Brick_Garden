bc4f1cfe08e4624c24db5234ea0f65544c52e3d9
Darius Khani <smp2qg@virginia.edu>
2026-09-06 20:08:32 -0400

    Variable plant_name and plant_stages

--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -2,7 +2,7 @@ import os
 
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, StringProperty
 
 # Loads home_screen.kv | Register HomeScreen Rule
 Builder.load_file(os.path.join(os.path.dirname(__file__), 'home_screen.kv'))    # Strips 'homescreen.py' from path string and replaces it with 'home_screen.kv'
@@ -10,4 +10,6 @@ Builder.load_file(os.path.join(os.path.dirname(__file__), 'home_screen.kv'))    
 # HomeScreen Widget
 class HomeScreen(Screen):
     vitality = NumericProperty(10)
+    plant_name = StringProperty("monstera")
+    plant_stage = NumericProperty(5)
     pass
